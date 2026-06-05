0c1871e6ea47a98fb5d7ca451f81174c247eabea
Loic ANGO <loicango11@gmail.com>
2026-06-05 10:35:59 +0100

    Activation de Vercel Analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next"
 import { Poppins, Roboto_Condensed, Fraunces } from "next/font/google";
 import "./globals.css";
 import CookieBanner from "@/app/components/CookieBanner";
